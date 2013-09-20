a43a82ac63f57e72e2809e25b3f2e946ba971e6d
Jeremy Reeve <jsr@argusat.com>
2013-09-20 18:51:40 +0100

    Add some fields to the internal Location representation

--- gjl-api-java/src/main/java/com/argusat/gjl/model/Location.java
+++ gjl-api-java/src/main/java/com/argusat/gjl/model/Location.java
@@ -20,8 +20,54 @@ import com.argusat.gjl.service.observation.ObservationProtoBuf;
 
 public class Location {
 
+	private float latitude;
+	
+	private float longitude;
+	
+	private float altitude;
+	
+	private float error;
+	
 	private ObservationProtoBuf.Observation.Location locationProtoBuf;
 	
+	public float getLatitude() {
+		return latitude;
+	}
+
+
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+
+
+	public float getLongitude() {
+		return longitude;
+	}
+
+
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+
+
+	public float getAltitude() {
+		return altitude;
+	}
+
+
+	public void setAltitude(float altitude) {
+		this.altitude = altitude;
+	}
+
+
+	public float getError() {
+		return error;
+	}
+
+
+	public void setError(float error) {
+		this.error = error;
+	}
 	
 	public ObservationProtoBuf.Observation.Location getProtoBuf() {
 		return locationProtoBuf;
