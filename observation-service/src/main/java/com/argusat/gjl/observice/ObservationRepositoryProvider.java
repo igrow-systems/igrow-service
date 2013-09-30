921368f83d0f2304a9c471b12ae4fd4b969f4123
Jeremy Reeve <jsr@argusat.com>
2013-09-30 09:54:52 +0100

    fix build (again) :-(

--- observation-service/src/main/java/com/argusat/gjl/observice/ObservationRepositoryProvider.java
+++ observation-service/src/main/java/com/argusat/gjl/observice/ObservationRepositoryProvider.java
@@ -16,35 +16,36 @@
 
 package com.argusat.gjl.observice;
 
-import javax.ws.rs.ext.Provider;
-
-import com.sun.jersey.core.spi.component.ComponentContext;
-import com.sun.jersey.core.spi.component.ComponentScope;
-import com.sun.jersey.spi.inject.Injectable;
-import com.sun.jersey.spi.inject.InjectableProvider;
-
-@Provider
-public class ObservationRepositoryProvider implements
-		InjectableProvider<Inject, Type> {
-
-	@Override
-	public ComponentScope getScope() {
-		return ComponentScope.PerRequest;
-	}
-
-	@Override
-	public Injectable getInjectable(final ComponentContext arg0,
-			final ObservationRepository arg1, final Type arg2) {
-		if (Integer.class.equals(arg2)) {
-			return new Injectable<Integer>() {
-
-				@Override
-				public Integer getValue() {
-					return new Integer(99);
-				}
-			};
-		} else {
-			return null;
-		}
-	}
-}
+//import javax.ws.rs.ext.Provider;
+//
+//import com.sun.jersey.core.spi.component.ComponentContext;
+//import com.sun.jersey.core.spi.component.ComponentScope;
+//import com.sun.jersey.spi.inject.Inject;
+//import com.sun.jersey.spi.inject.Injectable;
+//import com.sun.jersey.spi.inject.InjectableProvider;
+//
+//@Provider
+//public class ObservationRepositoryProvider implements
+//		InjectableProvider<Inject, Type> {
+//
+//	@Override
+//	public ComponentScope getScope() {
+//		return ComponentScope.PerRequest;
+//	}
+//
+//	@Override
+//	public Injectable getInjectable(final ComponentContext arg0,
+//			final ObservationRepository arg1, final Type arg2) {
+//		if (Integer.class.equals(arg2)) {
+//			return new Injectable<Integer>() {
+//
+//				@Override
+//				public Integer getValue() {
+//					return new Integer(99);
+//				}
+//			};
+//		} else {
+//			return null;
+//		}
+//	}
+//}
