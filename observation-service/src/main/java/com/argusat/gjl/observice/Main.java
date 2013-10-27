03301111a9932a760f289ee4634b2cede9227b2c
Jeremy Reeve <jsr@argusat.com>
2013-10-27 09:02:02 +0000

    Change server uri from localhost to 0.0.0.0 in order that it listens to external network interface

--- observation-service/src/main/java/com/argusat/gjl/observice/Main.java
+++ observation-service/src/main/java/com/argusat/gjl/observice/Main.java
@@ -41,7 +41,7 @@ public class Main {
     }
 
     private static URI getBaseURI() {
-        return UriBuilder.fromUri("http://localhost/").port(getPort(9998)).build();
+        return UriBuilder.fromUri("http://0.0.0.0/").port(getPort(9998)).build();
     }
 
     public static final URI BASE_URI = getBaseURI();
