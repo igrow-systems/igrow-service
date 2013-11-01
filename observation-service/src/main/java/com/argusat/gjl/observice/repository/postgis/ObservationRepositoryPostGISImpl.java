c766057dc22b1e7a000f5b927d30e43772ed7587
Jeremy Reeve <jsr@argusat.com>
2013-11-01 08:22:48 +0000

    immaterial organisation of imports

--- observation-service/src/main/java/com/argusat/gjl/observice/repository/postgis/ObservationRepositoryPostGISImpl.java
+++ observation-service/src/main/java/com/argusat/gjl/observice/repository/postgis/ObservationRepositoryPostGISImpl.java
@@ -26,12 +26,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import org.postgis.PGgeometry;
-import org.postgis.PGgeometryLW;
 import org.postgis.Point;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.argusat.gjl.model.Observation;
 import com.argusat.gjl.model.Observation.ObservationType;
