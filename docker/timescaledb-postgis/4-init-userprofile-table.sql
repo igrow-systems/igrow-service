e60650a6281dc6907976d14f1541d98cee325216
Jeremy Reeve <jeremy.reeve81@gmail.com>
2018-01-28 10:59:48 +1300

    Add SQL init scripts for userprofile and userprofile-device mapping

---
+++ docker/timescaledb-postgis/4-init-userprofile-table.sql
@@ -0,0 +1,7 @@
+
+DROP TABLE IF EXISTS userprofile;
+
+CREATE TABLE userprofile (
+    userprofile_id      varchar(255) NOT NULL,    
+    email_address       varchar(255) NOT NULL
+);
---
+++ docker/timescaledb-postgis/5-init-userprofile-device-table.sql
@@ -0,0 +1,7 @@
+
+DROP TABLE IF EXISTS userprofile_device;
+
+CREATE TABLE userprofile_device (
+    userprofile_id varchar(255) NOT NULL,    
+    device_id      varchar(255) NOT NULL
+);
